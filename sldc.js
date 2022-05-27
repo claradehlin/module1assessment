1fc647e2784990824c937abeb775d498c1ea46c7
Clara Dehlin <claramargaretdehlin@gmail.com>
2022-05-27 10:18:03 -0600

    finished first section

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Testing & Integration
+// Implementation
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+
+let planning = ('Project leaders evaluate the terms of the project, including calculating labor and material costs, creating a timeline with target goals, and creating project teams and leadership structure.')
+let analysisOfRequirements = ('Determine what the application is supposed to do and its requirements. This step also involves defining the resources needed to build the project.')
+let design = ('This step models the way that a software application will work, including architecture, user interface, platforms, programming, communications, and security.')
+let testingAndIntegration= ('Testing the application before making it available to users is very important. This involves automated testing, but also creating simulated production environments for compex deployments. This step also reduces the number of bugs and glitches that users will encounter.')
+let implementation = ('In this step, the system is installed to support the intended business functions, after the system has been tested and accepted by the user. This includes installation and user notification/training.')
+let maintain = ('Though the development cycle is almost finished, users will still discover bugs that were not found during testing that will need to be resolved. This step can also involve planning for additional features in future releases.')
